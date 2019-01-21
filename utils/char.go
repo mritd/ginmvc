6502e46f15cd653421cb3d2c82e55ace88661d5f
mritd <mritd1234@gmail.com>
2019-01-21 22:41:40 +0800

    feat(utils): fix rand
    fix rand
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- utils/char.go
+++ utils/char.go
@@ -14,6 +14,7 @@ const (
 )
 
 func RandString(n int) string {
+	rand.Seed(time.Now().UnixNano())
 	b := make([]byte, n)
 	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
 	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
