8f4fb38807e88a09ccc64b6cf01449dbd5fca087
mritd <mritd1234@gmail.com>
2019-02-10 20:31:08 +0800

    style(utils): remove unused func
    remove unused func
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- utils/char.go
+++ utils/char.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"bytes"
 	"math/rand"
 	"time"
 )
@@ -30,42 +29,4 @@ func RandString(n int) string {
 	}
 
 	return string(b)
-}
-
-// 指定长度随机中文字符(包含复杂字符)
-func GenFixedLengthChineseChars(length int) string {
-
-	var buf bytes.Buffer
-
-	for i := 0; i < length; i++ {
-		buf.WriteRune(rune(RandInt(19968, 40869)))
-	}
-	return buf.String()
-}
-
-// 指定范围随机中文字符
-func GenRandomLengthChineseChars(start, end int) string {
-	length := RandInt(start, end)
-	return GenFixedLengthChineseChars(length)
-}
-
-// 指定范围随机 int
-func RandInt(min, max int) int {
-	rand.Seed(time.Now().UnixNano())
-	return min + rand.Intn(max-min)
-}
-
-// 指定范围随机 int64
-func RandInt64(min, max int64) int64 {
-	rand.Seed(time.Now().UnixNano())
-	return min + rand.Int63n(max-min)
-}
-
-// 反转字符串
-func ReverseString(s string) string {
-	runes := []rune(s)
-	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
-		runes[from], runes[to] = runes[to], runes[from]
-	}
-	return string(runes)
 }
